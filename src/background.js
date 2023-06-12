ff54875728e4c32a81c7f7900d817df1a48e4ae3
zaki3mymy <zaki.last@gmail.com>
2023-06-12 17:46:05 +0900

    prettier を実施

--- src/background.js
+++ src/background.js
@@ -1,30 +1,33 @@
-const parent = chrome.contextMenus.create({
+const parent = chrome.contextMenus.create(
+  {
     id: "transpose-words",
     title: "選択文字を縦書きでコピー",
     contexts: ["all"],
-}, () => chrome.runtime.lastError);
+  },
+  () => chrome.runtime.lastError
+);
 
 // click event
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    switch (info.menuItemId) {
-        case "transpose-words":
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: main,
-            });
-            break;
-    }
+  switch (info.menuItemId) {
+    case "transpose-words":
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: main,
+      });
+      break;
+  }
 });
 
 const main = () => {
-    // 選択文字の取得
-    const selectedText = window.getSelection().toString();
+  // 選択文字の取得
+  const selectedText = window.getSelection().toString();
 
-    // 回転処理
-    const rotatedText = rotate(selectedText)
+  // 回転処理
+  const rotatedText = rotate(selectedText);
 
-    // クリップボードへ書き込む
-    if(navigator.clipboard){
-        navigator.clipboard.writeText(rotatedText);
-    }
+  // クリップボードへ書き込む
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(rotatedText);
+  }
 };
--- src/common.js
+++ src/common.js
@@ -1,36 +1,36 @@
 const FILLSTRING = "　";
 
 const rotate = (text) => {
-    const separatedText = text.split("\n");
+  const separatedText = text.split("\n");
 
-    // 回転処理のために空白で埋める
-    // | あい |   → | あい　 |
-    // | うえお | → | うえお |
-    const maxLength = Math.max(...separatedText.map(t => t.length));
-    const complemented = []
-    separatedText.forEach(t => {
-        complemented.push(t.padEnd(maxLength, FILLSTRING))
-    })
-    // 文字列を全て分割して二次元配列にする
-    // [
-    //    ["あ", "い", "　"],
-    //    ["う", "え", "お"],
-    // ]
-    const matrix = complemented.map(t => t.split(""))
+  // 回転処理のために空白で埋める
+  // | あい |   → | あい　 |
+  // | うえお | → | うえお |
+  const maxLength = Math.max(...separatedText.map((t) => t.length));
+  const complemented = [];
+  separatedText.forEach((t) => {
+    complemented.push(t.padEnd(maxLength, FILLSTRING));
+  });
+  // 文字列を全て分割して二次元配列にする
+  // [
+  //    ["あ", "い", "　"],
+  //    ["う", "え", "お"],
+  // ]
+  const matrix = complemented.map((t) => t.split(""));
 
-    // 右に90度回転（配列を転置してから各行を逆向きに並び替える）
-    // [                    [
-    //   ["あ", "う"],         ["う", "あ"],
-    //   ["い", "え"],   →     ["え", "い"],
-    //   ["　", "お"]          ["お", "　"]
-    // ]                    ]
-    const _rotate = arr => arr[0].map((_, c) => arr.map(r => r[c]).reverse());
-    const rotatedMatrix = _rotate(matrix)
+  // 右に90度回転（配列を転置してから各行を逆向きに並び替える）
+  // [                    [
+  //   ["あ", "う"],         ["う", "あ"],
+  //   ["い", "え"],   →     ["え", "い"],
+  //   ["　", "お"]          ["お", "　"]
+  // ]                    ]
+  const _rotate = (arr) => arr[0].map((_, c) => arr.map((r) => r[c]).reverse());
+  const rotatedMatrix = _rotate(matrix);
 
-    // 回転した配列を結合
-    const rotatedText = rotatedMatrix.map(arr => arr.join("")).join("\n")
+  // 回転した配列を結合
+  const rotatedText = rotatedMatrix.map((arr) => arr.join("")).join("\n");
 
-    return rotatedText;
-}
+  return rotatedText;
+};
 
 module.exports = { rotate };
